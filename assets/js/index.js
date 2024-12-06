d49c7831be5ab6af73fe6061758bc7254e6a2c5d
kamleshbaheti <kamleshbaheti2232@gmail.com>
2024-12-06 23:57:25 +0530

    Updated portfolio and fixed lags

--- assets/js/index.js
+++ assets/js/index.js
@@ -17364,14 +17364,14 @@ document.addEventListener("contextmenu", function (e) {
 	e.preventDefault();
 });
 
-// document.addEventListener("keydown", function (event) {
-// 	if (event.ctrlKey && event.shiftKey && event.key.toLowerCase() === "i") {
-// 		event.preventDefault();
-// 	}
-// 	if (event.ctrlKey && event.key.toLowerCase() === "u") {
-// 		event.preventDefault();
-// 	}
-// });
+document.addEventListener("keydown", function (event) {
+	if (event.ctrlKey && event.shiftKey && event.key.toLowerCase() === "i") {
+		event.preventDefault();
+	}
+	if (event.ctrlKey && event.key.toLowerCase() === "u") {
+		event.preventDefault();
+	}
+});
 
 let originalTitle = document.title;
 const titles = [

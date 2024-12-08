b73538049405d5c3e035d6f5fd8c9c851915da2c
kamleshbaheti <kamleshbaheti2232@gmail.com>
2024-12-08 18:04:39 +0530

    Fine tuned animations and sound

--- assets/js/index.js
+++ assets/js/index.js
@@ -17371,6 +17371,9 @@ document.addEventListener("keydown", function (event) {
 	if (event.ctrlKey && event.key.toLowerCase() === "u") {
 		event.preventDefault();
 	}
+	if (event.key === "F12") {
+		event.preventDefault();
+	}
 });
 
 let originalTitle = document.title;

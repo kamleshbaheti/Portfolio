7aacdd46ac907270cc10e1830c71f48c033ed2f6
kamleshbaheti <kamleshbaheti2232@gmail.com>
2025-01-13 13:16:23 +0530

    Modified backend security

--- assets/js/every.js
+++ assets/js/every.js
@@ -147,17 +147,17 @@ function animateWord() {
 
 animateWord();
 
-// document.addEventListener("keydown", function (event) {
-// 	if (event.ctrlKey && event.shiftKey && event.key.toLowerCase() === "i") {
-// 		event.preventDefault();
-// 	}
-// 	if (event.ctrlKey && event.key.toLowerCase() === "u") {
-// 		event.preventDefault();
-// 	}
-// 	if (event.key === "F12") {
-// 		event.preventDefault();
-// 	}
-// });
+document.addEventListener("keydown", function (event) {
+	if (event.ctrlKey && event.shiftKey && event.key.toLowerCase() === "i") {
+		event.preventDefault();
+	}
+	if (event.ctrlKey && event.key.toLowerCase() === "u") {
+		event.preventDefault();
+	}
+	if (event.key === "F12") {
+		event.preventDefault();
+	}
+});
 
 let originalTitle = document.title;
 const titles = [
